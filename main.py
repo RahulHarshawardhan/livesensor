14c047ab5ae0a8d2b05c0d7e4b830ac3a8cbe286
Rahul Harshawardhan <rahulharshawardhan@yahoo.co.in>
2024-07-02 15:07:13 +0530

    logger.py created

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from sensor.exception import SensorException
 import os
 import sys
+from sensor.logger import logging
+
 
 def test_exception():
     try:
+        logging.info("There will be a error which is division by zero error")
         a = 1/0
     except Exception as e:
         raise SensorException(e, sys)
--- sensor/logger.py
+++ sensor/logger.py
@@ -4,4 +4,18 @@ import sys
 from datetime import datetime
 
 
+LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log" 
+
+logs_dir = os.path.join(os.getcwd(),"logs") # logs is the folder and joinning with LOG_FIle. 
+                                   #os.getcwd() will locate the directory where the error has come.
+os.makedirs(logs_dir, exist_ok= True) # This command will ensure that once the folder is created, it 
+# will not be deleted. If the folder is already created, it will not create again.
+LOG_FILE_PATH = os.path.join(logs_dir, LOG_FILE)
+
+
+logging.basicConfig(
+    filename = LOG_FILE_PATH,
+    level = logging.INFO,
+    format = '[%(asctime)s] %(lineno)d  %(name)s - %(levelname)s - %(message)s'
+    )
 
